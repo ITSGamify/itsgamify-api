dc8c597b6e457b3985240f160f03a4628b330292
Quang-Thang <loquangthang07@gmail.com>
2025-06-22 12:21:11 +0700

    add  ci

--- src/its.gamify.api/Program.cs
+++ src/its.gamify.api/Program.cs
@@ -1,4 +1,5 @@
 using its.gamify.api;
+using its.gamify.api.Middlewares;
 using its.gamify.core.Mappers;
 using its.gamify.domains.Models;
 
@@ -20,7 +21,7 @@ builder.Services.AddRouting(x => x.LowercaseUrls = true);
 builder.Configuration.AddUserSecrets<Program>();
 var configuration = builder.Configuration
     .Get<AppSetting>() ?? throw new Exception("Null configuration");
-
+builder.Services.AddSingleton<GlobalErrorHandlingMiddleware>();
 builder.Services.AddSingleton(configuration);
 builder.Services.AddOpenApi();
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
@@ -29,12 +30,14 @@ builder.Services.AddAutoMapper(typeof(MapperConfigurationProfile).Assembly);
 var app = builder.Build();
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
+app.UseMiddleware<GlobalErrorHandlingMiddleware>();
+app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 app.MapOpenApi();
 
 
-app.UseCors("AllowAll");
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
